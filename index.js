4f5384abdc1a368e391a29dadbeba5a27784023f
lalilaloe <lalilaloe@hotmail.com>
2020-09-01 07:45:59 +0200

    Color screen

--- index.js
+++ index.js
@@ -1,32 +1,43 @@
 const { Board, Proximity } = require("johnny-five");
-const board = new Board();
+const board = new Board({
+    port: "COM5"
+  });
 
 const distance = 20;
-let redCar = false;
+let redCarPassed = false;
 let redCarTimer;
-let blackCar = false;
+let blackCarPassed = false;
 let blackCarTimer;
 const figlet = require('figlet')
+const chalk = require('chalk')
 
-function startStopLap(carPassed, carTimer) {
+function startStopLap(carPassed, carTimer, color = false) {
     if (!carPassed) {
-        console.log("Start red lap")
+        // if(color){
+        //     console.log(chalk.bgRed("Start lap"));
+        // }else{
+        //     console.log(chalk.black.bgWhite("Start lap"));
+        // }   
         carTimer = new Date()
         carPassed = true;
     } else {
         var end = new Date() - carTimer
         if (end > 1000) {
-            console.info('Red car lap time: %dms', end)
-            figlet(end / 1000);
+            if(color){
+                console.log(chalk.bgRed(figlet.textSync((end / 1000), { horizontalLayout: 'full' })));
+            }else{
+                console.log(chalk.black.bgWhite(figlet.textSync((end / 1000), { horizontalLayout: 'full' })));
+            }            
             carTimer = new Date()
             carPassed = false;
         }
     }
-    console.log("----")
+    process.stdout.write("-")
     return { carPassed: carPassed, carTimer: carTimer }
 }
 
 board.on("ready", () => {
+    console.log("Ready")
     const redCarSensor = new Proximity({
         controller: "HCSR04",
         pin: 13
@@ -40,8 +51,8 @@ board.on("ready", () => {
     redCarSensor.on("change", () => {
         const { centimeters } = redCarSensor;
         if (centimeters < distance) {
-            const { carPassed, carTimer } = startStopLap(redCar, redCarTimer)
-            redCar = carPassed;
+            const { carPassed, carTimer } = startStopLap(redCarPassed, redCarTimer, true)
+            redCarPassed = carPassed;
             redCarTimer = carTimer;
         }
     });
@@ -49,8 +60,8 @@ board.on("ready", () => {
     blackCarSensor.on("change", () => {
         const { centimeters } = blackCarSensor;
         if (centimeters < distance) {
-            const { carPassed, carTimer } = startStopLap(blackCar, blackCarTimer)
-            blackCar = carPassed;
+            const { carPassed, carTimer } = startStopLap(blackCarPassed, blackCarTimer)
+            blackCarPassed = carPassed;
             blackCarTimer = carTimer;
         }
     });
---
+++ readme.md
@@ -0,0 +1,2 @@
+npm install nodebots-interchange -g
+interchange install hc-sr04 -a uno -p COM4 --firmata
